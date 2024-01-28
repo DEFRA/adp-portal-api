a0fa76783ce807636a647f0afecc1075bf996048
Praveen Potturi <praveen.potturi@gmail.com>
2024-01-28 21:41:15 +0000

    Updates

--- src/ADP.Portal.Api/Providers/VssConnectionProvider.cs
+++ src/ADP.Portal.Api/Providers/VssConnectionProvider.cs
@@ -28,7 +28,7 @@ namespace ADP.Portal.Api.Providers
                 var patToken = await GetPatTokenAsync(keyValutName, adoConfig);
                 if (string.IsNullOrEmpty(patToken))
                 {
-                    throw new ArgumentNullException("patToken");
+                    throw new Exception("PAT token is empty");
                 }
                 connection = new VssConnectionWrapper(new Uri(adoConfig.OrganizationUrl), new VssBasicCredential(string.Empty, patToken));
             }
