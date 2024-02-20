290316880c82de7b994c13ea15898b7942991ed4
Santosh Kumar Mohapatra <santosh.mohapatra@capgemini.com>
2024-02-20 17:34:29 +0000

    removed warning

--- src/ADP.Portal.Api/Services/GraphClient.cs
+++ src/ADP.Portal.Api/Services/GraphClient.cs
@@ -27,36 +27,29 @@ namespace ADP.Portal.Core.Ado.Services
         public async Task<GraphServiceClient> GetServiceClient()
         {      
             string authority = $"{serviceConnectcion.Instance}{serviceConnectcion.TenantId}";
-            try
+    
+            AuthenticationContext _authContext = new AuthenticationContext(authority);
+            ClientCredential _clientCred = new ClientCredential(serviceConnectcion.ClientId, serviceConnectcion.ClientSecret);
+
+            //Getting bearer token for Authentication
+            AuthenticationResult _authResult = await _authContext.AcquireTokenAsync(serviceConnectcion.GraphResource, _clientCred);
+            var token = _authResult.AccessToken;
+
+            //Getting Graphclient API Endpoints
+            var _authProvider = new DelegateAuthenticationProvider((requestMessage) =>
             {
-                AuthenticationContext _authContext = new AuthenticationContext(authority);
-                ClientCredential _clientCred = new ClientCredential(serviceConnectcion.ClientId, serviceConnectcion.ClientSecret);
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token.ToString());
+                return Task.FromResult(0);
+            });           
 
-                //Getting bearer token for Authentication
-                AuthenticationResult _authResult = await _authContext.AcquireTokenAsync(serviceConnectcion.GraphResource, _clientCred);
-                var token = _authResult.AccessToken;
+            string? graphAPIEndpoint = $"{serviceConnectcion.GraphResource}{serviceConnectcion.GraphResourceEndPoint}";
 
-                //Getting Graphclient API Endpoints
-                var _authProvider = new DelegateAuthenticationProvider((requestMessage) =>
-                {
-                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token.ToString());
-                    return Task.FromResult(0);
-                });
-
-                string? graphAPIEndpoint = $"{serviceConnectcion.GraphResource}{serviceConnectcion.GraphResourceEndPoint}";
-
-                // Initializing the GraphServiceClient with Authentication
-                graphClient = new GraphServiceClient(graphAPIEndpoint, _authProvider);
-                return graphClient;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
-            }
+            // Initializing the GraphServiceClient with Authentication
+            graphClient = new GraphServiceClient(graphAPIEndpoint, _authProvider);
+            return graphClient;     
             
         }
-        public string GetGroupId()
+        public string? GetGroupId()
         {
             return serviceConnectcion.GroupId;
         }
