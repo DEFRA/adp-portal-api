9980494499d9ade1c95501fee612dc2cbd1f6509
Santosh Kumar Mohapatra <santosh.mohapatra@capgemini.com>
2024-02-21 12:05:10 +0000

    Added Unit Test

---
+++ test/ADP.Portal.Api.Tests/Services/GraphClientTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ADP.Portal.Core.Ado.Services;
+
+namespace ADP.Portal.Api.Tests.Services
+{
+    [TestFixture]
+    public class GraphClientTests
+    {
+        
+        [Test]
+        public async Task GetServiceClient_ReturnNull_WhenDoesNotExist()
+        {
+            //Arrange
+           var client = new GraphClient();
+
+            // Act
+            var result = await client.GetServiceClient();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+        
+        [Test]
+        public async Task GetServiceClient_ReturnClient_WhenExist()
+        {
+            //Arrange
+            GraphClient client = new GraphClient();
+
+            // Act
+            var result = await client.GetServiceClient();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }       
+    }
+}
