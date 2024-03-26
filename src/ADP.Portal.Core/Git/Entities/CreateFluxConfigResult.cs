e56408c0167eb5dce0ae9163f17ffb79844ff434
Ajay Soni <ajay.b.soni@capgemini.com>
2024-03-26 19:07:33 +0000

    Clean up.

--- src/ADP.Portal.Core/Git/Entities/CreateFluxConfigResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADP.Portal.Core.Git.Entities
-{
-    public class CreateFluxConfigResult
-    {
-        public bool IsConfigExists { get; set; } = true;
-
-        public List<string> Errors { get; set; } = [];
-    }
-}
