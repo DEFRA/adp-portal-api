a12787837263176ede28c6904de0302f196a589a
James Coxhead <james.coxhead@capgemini.com>
2024-05-20 17:49:30 +0100

    fix: Sonar bugs

--- src/ADP.Portal.Api/Models/Group/SetGroupMembersRequest.cs
+++ src/ADP.Portal.Api/Models/Group/SetGroupMembersRequest.cs
@@ -1,8 +1,15 @@
-﻿namespace ADP.Portal.Api.Models.Group;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADP.Portal.Api.Models.Group;
 
 public class SetGroupMembersRequest
 {
+    [Required]
     public required IEnumerable<string> TechUserMembers { get; set; }
+
+    [Required]
     public required IEnumerable<string> NonTechUserMembers { get; set; }
+
+    [Required]
     public required IEnumerable<string> AdminMembers { get; set; }
 }
